9a2d5caa8408bb826faa27b499e61b773120a172
iesiyok <iesiyok@student.unimelb.edu.au>
2015-08-13 15:57:53 +1000

    initial project repository

---
+++ manucapture-image-lib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manucapture_image_lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Manucapture Image Library");
+        }
+    }
+}
